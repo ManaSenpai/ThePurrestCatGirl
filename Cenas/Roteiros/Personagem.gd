43c9133048b605e681415471271fcbeb78330665
ManaSenpai <manabestgirl@gmail.com>
2020-04-18 20:52:17 -0300

    Resolvido errinho que fazia o personagem atravessar a parede, purrfect!

--- Cenas/Roteiros/Personagem.gd
+++ Cenas/Roteiros/Personagem.gd
@@ -3,47 +3,42 @@ extends Area2D
 var direcao = Vector2(1,0)
 var VEL = 800
 var velocidade = 0
+var pode_mover = false
+
 
 func _physics_process(delta):
-	if(Input.is_action_just_pressed("espaco")):
-		velocidade = VEL
-		$TimerRotacao.stop()
+	if (velocidade == 0):
+		if (Input.is_action_just_pressed("ui_up")):
+			direcao = muda_direcao(0)
+		elif (Input.is_action_just_pressed("ui_right")):
+			direcao = muda_direcao (90)
+		elif (Input.is_action_just_pressed("ui_down")):
+			direcao = muda_direcao (180)
+		elif (Input.is_action_just_pressed("ui_left")):
+			direcao = muda_direcao (270)
 	position += direcao.normalized() * delta * velocidade
+	
 
 
-func _on_TimerRotacao_timeout():
 
-	rotation_degrees += 90
-	if (rotation_degrees >= 360):
-		rotation_degrees = 0
-	direcao = define_direcao(rotation_degrees)
-
-func define_direcao(rotacao):
+func muda_direcao(rotacao):
 	var  d = [
 		Vector2(0,-1),
 		Vector2(1,0),
 		Vector2(0,1),
 		Vector2(-1,0)
 	]
+	rotation_degrees = rotacao
+	velocidade = VEL
 	return d[rotacao/90]
 
-
-func _on_VisibilityNotifier2D_screen_exited():
-	position = Vector2(500,300)
-	para()
-	pass 
 
 func para():
 	velocidade = 0
 	var posicao64 = position/64
 	posicao64 = posicao64.round()
-	
 	position = posicao64*64
 	
-	print(position)
-	$TimerRotacao.start()
-
 func _on_Personagem_area_entered(area):
 	if ("Parede" in area.name):
 		para()
-	

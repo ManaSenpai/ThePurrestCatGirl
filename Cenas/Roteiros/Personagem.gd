a67f0a6c070e3c52ef5e764677bb285864b452cd
ManaSenpai <manabestgirl@gmail.com>
2020-04-26 21:26:43 -0300

    Tentativas e erros, preciso fazer um cenário bonitinho e acrescentei animações a purrsonagem

--- Cenas/Roteiros/Personagem.gd
+++ Cenas/Roteiros/Personagem.gd
@@ -3,7 +3,7 @@ extends Area2D
 # Variável responsável por dar a direção dos movimentos do jogador
 var direcao = Vector2(1,0)
 # Constante de velocidade
-var VEL = 800
+var VEL = 300
 # Variável responsável por dar o movimento ao jogador 
 var velocidade = 0
 
@@ -19,11 +19,15 @@ func _physics_process(delta):
 		elif (Input.is_action_just_pressed("ui_right")):
 			muda_direcao (90)
 		# Movimentação para baixo
+			if($AnimatedSprite.flip_h):
+				$AnimatedSprite.flip_h = false
 		elif (Input.is_action_just_pressed("ui_down")):
 			muda_direcao (180)
 		# Movimentação para a esquerda
 		elif (Input.is_action_just_pressed("ui_left")):
 			muda_direcao (270)
+			if(!$AnimatedSprite.flip_h):
+				$AnimatedSprite.flip_h = true
 	
 	"""
 	Formula para movimentar o personagem, baseado no 
@@ -35,6 +39,8 @@ func _physics_process(delta):
 
 # Função responsável por alterar a direção do personagem
 func muda_direcao(rotacao):
+	
+	$AnimatedSprite.animation = "slide"
 	# Array com as direções possíveis
 	var  d = [
 		Vector2(0,-1),
@@ -43,7 +49,7 @@ func muda_direcao(rotacao):
 		Vector2(-1,0)
 	]
 	# Muda a rotação do objeto
-	rotation_degrees = rotacao
+	$CollisionShape2D.rotation_degrees = rotacao
 	# Dá velocidade ao objeto
 	velocidade = VEL
 	# E atribui a direcao para que haja a movimentação
@@ -53,7 +59,7 @@ func muda_direcao(rotacao):
 func para():
 	# Zera a velocidade
 	velocidade = 0
-	
+	$AnimatedSprite.animation = "idle"
 	"""
 	Formula que ajusta a posição, 
 	considerando que ele anda apenas em 64 pixels
@@ -66,3 +72,9 @@ func _on_Personagem_area_entered(area):
 	# Se um objeto parede entrar em contato com ele, ele para
 	if ("Parede" in area.name):
 		para()
+
+
+func _on_AnimatedSprite_animation_finished():
+	if ($AnimatedSprite.animation == "slide"):
+		$AnimatedSprite.animation = "slide_loop"
+	pass # Replace with function body.
